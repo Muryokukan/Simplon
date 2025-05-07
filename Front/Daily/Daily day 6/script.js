e398c0a0954b5b99aa9ceddd12cdc1fff7ddec27
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-05-07 12:09:18 +0200

    Daily: First part done

--- Front/Daily/Daily day 6/script.js
+++ Front/Daily/Daily day 6/script.js
@@ -33,4 +33,61 @@
 // Modifiez la valeur de la variable prenom à "Paul".
 
 // Affichez les valeurs de prenom et de personne2.prenom.
-let 
+
+// let prenom = "Pierre";
+// let personne2 = {prenom: "Marie",};
+
+
+// console.log(prenom);
+
+// prenom = "Paul";
+
+// console.log(prenom);
+// console.log(personne2.prenom);
+
+// ---------------------------------------------------------------------------------------------------
+
+// Exercice 3 : Savoir itérer dans un objet
+// Créez un objet voiture avec les propriétés suivantes :
+
+// marque : "Toyota"
+// modele : "Corolla"
+// annee : 2015
+// Utilisez une boucle for...in pour itérer sur les propriétés de voiture et affichez chaque clé et valeur.
+
+// let voiture = {
+//     marque: "toyota",
+//     modele: "corolla",
+//     annee: "2015"
+// }
+
+// console.log(voiture.modele);
+
+// for (let propriete in voiture) {
+//     console.log(`${propriete} : ${voiture[propriete]}`)
+// };
+
+// ---------------------------------------------------------------------------------------------------
+
+// Exercice 4 : Savoir copier un objet (cloner)
+// Créez un objet original avec quelques propriétés.
+
+// Créez une copie de original ("en utilisant l'opérateur de décomposition ({...original}) nah") et stockez-la dans une variable copie.
+
+// Modifiez une propriété dans copie.
+
+// Affichez les deux objets pour vérifier que la modification n'affecte que copie.
+
+// let personne = {
+//     nom: "Jean",
+//     age: 30,
+//     ville: "Paris"
+// };
+
+// let copy = personne;
+
+// console.log(copy);
+
+// copy.age = 34;
+
+// console.log(copy);
