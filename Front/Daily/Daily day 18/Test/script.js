f7d8ea9b2bb0f679830751ad246eff6abffa085a
Muryokukan <c.limosin@outlook.fr>
2025-05-26 16:37:45 +0200

    Update script.js

--- Front/Daily/Daily day 18/Test/script.js
+++ Front/Daily/Daily day 18/Test/script.js
@@ -1,23 +1,32 @@
 class CompteBancaire {
     constructor(titulaire, soldeInitial = 0) {
-        
+        this.titulaire = titulaire;
+        this.solde = soldeInitial;
     }
     
     deposer(montant) {
-        
+        if (montant > 0) {
+            this.solde += montant;
+        } else {
+            console.log("Le montant doit être supérieur à 0 !");
+        }
     }
     
     retirer(montant) {
-        
+        if (montant > 0 && montant <= this.solde) {
+            this.solde -= montant
+            console.log(`${montant}e retiré !`)
+        }
     }
     
     afficherSolde() {
-        
+        return `Solde de: ${this.titulaire}: ${this.solde}€`;
     }
+    
 }
 
-// Test ton code :
+
 const monCompte = new CompteBancaire("Alice", 100);
 monCompte.deposer(50);
 monCompte.retirer(30);
-console.log(monCompte.afficherSolde()); // Doit afficher 120
+console.log(monCompte.afficherSolde());
