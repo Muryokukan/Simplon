3705d7ebbe9cc7c8624ae1e336cfd7bc2d338f16
Muryokukan <c.limosin@outlook.fr>
2025-05-30 13:53:00 +0200

    setup the nodeJS

--- NodeJS/Simplon/src/exercise1.ts
+++ NodeJS/Simplon/src/exercise1.ts
@@ -2,3 +2,29 @@ console.log("hello world")
 
 
 console.log("hello world")
+
+
+// // 1. Define the Student interface
+// interface Student {
+//     // TODO: Complete with name (string), age (number), grades (array of numbers)
+//     name: string;
+//     age: number;
+//     grades: Array<number>;
+// }
+
+// // 2. Function to calculate average of grades
+// function calculateAverage(grades: number[]): number {
+//     // TODO: Calculate and return the average
+//     return grades.length === 0 ? 0 : grades.reduce((a, b) => a + b, 0) / grades.length;
+// }
+
+// // 3. Function to check if student passed
+// function hasPassed(student: Student): boolean {
+//     // TODO: Return true if average >= 10
+//     let 
+
+// }
+
+// // 4. Create students and test
+// // TODO: Create 2-3 students with different grades
+// // TODO: Display their average and if they passed
