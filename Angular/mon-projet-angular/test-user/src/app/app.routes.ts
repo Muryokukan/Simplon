94116d15acaef756cfe0883e75c4300dc9476907
Cédric <134500848+Muryokukan@users.noreply.github.com>
2025-06-04 14:25:19 +0200

    daily: day 24 testing navigation

--- Angular/mon-projet-angular/test-user/src/app/app.routes.ts
+++ Angular/mon-projet-angular/test-user/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { User } from './user/user';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: 'user/:id', component: User }, // Route avec paramètre
+  { path: '**', redirectTo: '/home' } // Route par défaut (404)
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
